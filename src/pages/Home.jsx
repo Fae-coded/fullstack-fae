52a9ac99626d8ba5e5f534e21786dae8430bd203
Sophie Beech <219414866+Fae-coded@users.noreply.github.com>
2026-06-09 17:20:12 +0100

    Add cover image and being styling home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,16 @@ export function Home() {
 
     return(
         <div className="home">
-            {/* image here */}
-            <h1>Sophie Beech</h1>
-            <h2>Junior Full Stack Developer</h2>
-            <p>Brief intro to go here.</p>
+            <div className="home-cover-image">
+                {/* image here */}
+                <h1>Sophie Beech</h1>
+                <h2>Junior Full Stack Developer</h2>
+            </div>
+            <p>
+                Full Stack certified early-career Web Developer skilled in JavaScript and Python employing object oriented programming principles. 
+                Adept with React and Django frameworks including building REST APIs. 
+                Experienced support agent with proven skill in troubleshooting and problem solving gained through 7+ years of customer support and SaaS experience.
+            </p>
         </div>
     )
 };
